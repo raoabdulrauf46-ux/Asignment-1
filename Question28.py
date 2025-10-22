5e05a50c420301d706b0884a2e1acf5b32c20f8b
raoabdulrauf46-ux <raoabdulrauf46@gmail.com>
2025-10-22 12:16:11 -0700

    Add files via upload

---
+++ Question28.py
@@ -0,0 +1,16 @@
+import time
+import math
+
+def show_square_root(value):
+    print(math.sqrt(value))
+
+print("Square roots after delay:")
+
+time.sleep(1)
+show_square_root(16)
+
+time.sleep(1)
+show_square_root(100)
+
+time.sleep(1)
+show_square_root(25100)
