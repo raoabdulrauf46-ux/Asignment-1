6fd1d062d73907f44742637fd0772e17f68e1f2e
raoabdulrauf46-ux <raoabdulrauf46@gmail.com>
2025-10-22 12:05:33 -0700

    Add files via upload

---
+++ program_9.py
@@ -0,0 +1,6 @@
+alphabet = input("Enter any alphabet : ").lower()
+if alphabet in 'aeiou':
+    print("the alphabet you typed is a vowel.")
+else:
+    print("the alphabet you typed is not a vowel.")
+ 
