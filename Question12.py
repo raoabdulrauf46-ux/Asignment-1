be8e8b9a8fbcb3f0c778e9dfaa96f95cf9e9dbc0
raoabdulrauf46-ux <raoabdulrauf46@gmail.com>
2025-10-22 12:07:46 -0700

    Add files via upload

---
+++ Question12.py
@@ -0,0 +1,25 @@
+number_of_rows=5
+for a in range (1,number_of_rows + 1):
+    for b in range(a):
+        print("*",end=" ")
+    print()
+for a in range (number_of_rows - 1, 0, -1):
+    for b in range (a):
+        print("*",end=" ")       
+    print() 
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
