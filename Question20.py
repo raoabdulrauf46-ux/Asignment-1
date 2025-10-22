fe3cc50bfc3881a4582ce872cb070237e3478535
raoabdulrauf46-ux <raoabdulrauf46@gmail.com>
2025-10-22 12:13:04 -0700

    Add files via upload

---
+++ Question20.py
@@ -0,0 +1,20 @@
+for top_line in range(0,5):
+    print("*",end="") 
+
+for left_edge_1 in range(0,1):
+    print("\n*")
+
+for left_edge_2 in range(0,1):
+    print("*")
+
+for middle_line in range(0,3):
+    print("*",end="")
+
+for left_edge_3 in range(0,1):
+    print("\n*")
+
+for left_edge_4 in range(0,1):
+    print("*")
+
+for bottom_line in range(0,6):
+    print("*",end="")
